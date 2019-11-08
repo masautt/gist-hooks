cf8386242ea73621c3afb2679af955d84b289667
masautt <marek.sautter@gmail.com>
2019-11-08 13:39:17 -0800

    Added method to get oauth token

--- server.js
+++ server.js
@@ -1,46 +1,24 @@
-
-
-// Import the express lirbary
 const express = require('express')
-
-// Import the axios library, to make HTTP requests
 const axios = require('axios')
 
 require('dotenv').config()
 
-// This is the client ID and client secret that you obtained
-// while registering the application
 const clientID = process.env.GIST_CLIENT_ID;
 const clientSecret = process.env.GIST_CLIENT_SECRET;
+const PORT = process.env.PORT;
 
-// Create a new express application and use
-// the express static middleware, to serve all files
-// inside the public directory
 const app = express()
-app.use(express.static(__dirname + '/public'))
 
-app.get('/oauth/redirect', (req, res) => {
-  // The req.query object has the query params that
-  // were sent to this route. We want the `code` param
-  const requestToken = req.query.code
-  axios({
-    // make a POST request
+app.get("/auth", (req, res) => res.redirect(`https://github.com/login/oauth/authorize?client_id=${clientID}`));
+
+app.get('/oauth/redirect', (req, res) => axios(oauthParams).then(response => response.data.access_token));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+const oauthParams = {
     method: 'post',
-    // to the Github authentication API, with the client ID, client secret
-    // and request token
-    url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
-    // Set the content type header, so that we get the response in JSOn
+    url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${req.query.code}`,
     headers: {
-      accept: 'application/json'
+        accept: 'application/json'
     }
-  }).then((response) => {
-    // Once we get the response, extract the access token from
-    // the response body
-    const accessToken = response.data.access_token
-    // redirect the user to the welcome page, along with the access token
-    res.redirect(`/welcome.html?access_token=${accessToken}`)
-  })
-})
-
-// Start the server on port 8080
-app.listen(8080)
+}
